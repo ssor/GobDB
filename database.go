5baedc11bafd54c70b474d477abf9f50d01d4334
Adam Smith <iihtimsad@gmail.com>
2014-10-17 04:04:15 -0400

    Setup filtered encoder.

---
+++ database.go
@@ -0,0 +1,111 @@
+// Implements a persistant key-value store of gob-compatible
+// types. This is accomplished with a light wrapper around 
+// leveldb and Go's gob encoding library.
+package GobDB
+
+import (
+	"bytes"
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+
+// LevelDB wrapper.
+type DB struct {
+	internal *leveldb.DB
+	location string
+	// essentials *bytes.Buffer
+	encoder FilteredEncoder
+	prepared bool
+}
+
+
+// Returns unopened database at with given datafile.
+func At(path string) DB {
+	buffer := bytes.NewBuffer([]byte{})
+	return DB {
+		location: path,
+		essentials: buffer,
+		encoder: MakeFilteredEncoder(buffer),
+	}
+}
+
+
+// Opens database if not done already.
+func (db *DB) Open() error {
+	if db.IsOpen() {
+		return nil
+	}
+
+	ret, err := leveldb.OpenFile(db.location, nil)
+	if err == nil {
+		db.internal = ret
+		db.prepareEncoder()
+	}
+	return err
+}
+
+
+// 
+func (db DB) IsOpen() bool {
+	return db.internal != nil
+}
+
+
+// Closes database if not done already.
+func (db *DB) Close() {
+	if db.IsOpen() {
+		db.internal.Close()
+		db.internal = nil
+	}
+}
+
+
+// // Encodes given key and value through gob, inserting resulting
+// // byte slices into the database's internal leveldb.
+// func (db *DB) Put(key, value interface{}) error {
+// 	if !db.IsOpen() {
+// 		return 
+// 	}
+// }
+
+
+// // Encodes given key via gob, fetches the corresponding value from
+// // within leveldb, and decodes that value into parameter two.
+// func (db DB) Get(key, value interface{}) error {
+// 	//
+// }
+
+
+// // Encodes given key via gob and checks if the resulting byte 
+// // slice exists in the database's internal leveldb.
+// func (db DB) Contains(key interface{}) bool {
+
+// }
+
+
+// // Encodes given key via gob, deleting the resulting byte slice 
+// // from the database's internal leveldb.
+// func (db *DB) Delete(key interface{}) error {
+
+// }
+
+
+
+// When the database is opened for the first time, scrolls through
+// all entries to form the same encoder that was used before the 
+// previous db.Close() call.
+//
+// Note that this is an expensive operation, scaling linearly with
+// dataset size. Accordingly, you should utilize the open and close
+// methods instead of initializing new DB objects all the time. For 
+// each initialization, the decoder is literally thrown in the 
+// garbage.
+func (db *DB) prepareEncoder() error {
+	if db.prepared == false {
+
+		db.prepared = true
+	}
+	return nil
+}
+
+
---
+++ encoder.go
@@ -0,0 +1,68 @@
+package GobDB
+
+import (
+	"io"
+	"bytes"
+	"encoding/gob"
+)
+
+
+// Wraps gob.Encoder in such a way to retain all of gobs internal
+// type definitions, and to return byte slices of type-value
+// pairs without any additional data.
+type FilteredEncoder struct {
+	essentials []interface{}
+	encoder *gob.Encoder
+	buffer *bytes.Buffer
+	writer io.Writer
+}
+
+
+// Constructs filter with an empty buffer and unused encoder. Only
+// data essential to the construction of new encodes are kept. This
+// consists of internal gob type definitions and one object of each
+// type.
+func MakeFilteredEncoder(w io.Writer) FilteredEncoder {
+	var r FilteredEncoder
+	r.writer = w // where essential data is outputted
+	r.buffer = bytes.NewBuffer([]byte{}) // buffer for temporary use
+	r.encoder = gob.NewEncoder(r.buffer) // wrapped gob encoder
+	return r
+}
+
+
+
+
+
+
+func (f *FilteredEncoder) Encode(e interface{}) ([]byte, error) {	
+	// Empty write buffer.
+	f.buffer.Reset()
+
+	// Encode value and its size.
+	err := f.encoder.Encode(e)
+	if err != nil {
+		return []byte{}, err
+	}
+	s1 := f.buffer.Len()
+
+	// Repeat.
+	err = f.encoder.Encode(e)
+	if err != nil {
+		return []byte{}, err
+	}
+	s2 := f.buffer.Len() - s1
+
+	// Infer whether or not a new type was written. If block sizes
+	// are equal, we know that the type being encoded has was 
+	// encoded beforehand. Otherwise, we know that the parameter to
+	// this function call is the first of its type to be encoded 
+	// through this filter.
+	r := make([]byte, s2)
+	copy(r, f.buffer.Bytes()[s1:])
+	if s1 != s2 {
+		f.writer.Write(f.buffer.Bytes())
+	}
+	return r, nil
+}
+
